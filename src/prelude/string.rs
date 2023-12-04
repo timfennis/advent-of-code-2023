3e4c0aeb2a77524c3ceb24d9c94790a8b9adb1c7
Tim Fennis <tim.fennis@nos.nl>
2023-12-04 08:41:29 +0100

    day 4 the smart solution

--- src/prelude/string.rs
+++ src/prelude/string.rs
@@ -9,7 +9,6 @@ impl StringTools for &str {
         let mut result = Vec::new();
 
         for c in self.chars() {
-
             if let Some(digit) = c.to_digit(10) {
                 if let Some(num) = current_num.as_mut() {
                     *num *= 10;
--- src/year2023/day4.rs
+++ src/year2023/day4.rs
@@ -1,69 +1,53 @@
-use itertools::Itertools;
 use std::cmp::min;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
+use crate::prelude::StringTools;
 use crate::puzzle::Answerable;
 use crate::{create_solution, Solution};
-use crate::prelude::StringTools;
 
 create_solution!(Day4, 2023, 4);
 impl Solution for Day4 {
     fn handle_input(&mut self, input: &str) -> anyhow::Result<()> {
+        let mut games: Vec<usize> = Vec::with_capacity(input.lines().count());
+
         let mut part1_answer = 0;
-
-        let mut games: HashMap<u32, u32> = Default::default();
         for line in input.lines() {
-            let (game, data) = line.split_once(": ").unwrap();
-            let (winning, my_numbers) = data.split_once(" | ").unwrap();
-            let (_, id) = game.split_once(' ').unwrap();
-            let id = id.trim().parse::<u32>().unwrap();
+            let (_game, data) = line.split_once(": ").expect("line must contain a ':'");
+            let (winning, my_numbers) = data.split_once(" | ").expect("line must contain '|'");
 
-            let winning = winning
-                .nums()
-                .into_iter()
-                .collect::<HashSet<_>>();
+            let winning = winning.nums().into_iter().collect::<HashSet<_>>();
+            let my_numbers = my_numbers.nums().into_iter().collect::<HashSet<_>>();
 
-            let my_numbers = my_numbers
-                .nums()
-                .into_iter()
-                .collect::<HashSet<_>>();
+            let win_count = my_numbers.intersection(&winning).count();
 
-            let win_count = my_numbers.intersection(&winning).count() as u32;
+            part1_answer += match win_count {
+                0 => 0,
+                _ => 2u32.pow((win_count - 1) as u32),
+            };
 
-            part1_answer += calc_score(win_count);
-
-            games.insert(id, win_count);
+            games.push(win_count);
         }
 
         self.submit_part1(part1_answer);
 
         assert_eq!(part1_answer, 20117);
 
-        let mut buf = (1..=198).collect_vec();
+        let mut cards = [1usize; 198];
 
-        let mut part2_answer = 0;
-        while let Some(game_id) = buf.pop() {
-            let win_count = games.get(&game_id).unwrap();
-
-            part2_answer += 1;
+        for (game_id, win_count) in games.into_iter().enumerate() {
             let new_range = (game_id + 1)..=min(game_id + win_count, 198);
+
             for n in new_range {
-                buf.push(n);
+                cards[n] += cards[game_id];
             }
         }
+
+        let part2_answer = cards.into_iter().sum::<usize>();
 
         self.submit_part2(part2_answer);
 
         assert_eq!(part2_answer, 13768818);
 
         Ok(())
-    }
-}
-
-fn calc_score(win_count: u32) -> u32 {
-    match win_count {
-        0 => 0,
-        1 => 1,
-        _ => 2u32.pow(win_count - 1),
     }
 }
