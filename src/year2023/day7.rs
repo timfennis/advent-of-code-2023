e38ee18a294e1bb434ddd5d7755446da224e0f3b
Tim Fennis <tim.fennis@nos.nl>
2023-12-07 07:26:35 +0100

    part 1 and 2

--- src/year2023/day7.rs
+++ src/year2023/day7.rs
@@ -6,35 +6,40 @@ use std::collections::HashMap;
 use std::str::FromStr;
 create_solution!(Day7, 2023, 7);
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug)]
 struct Hand {
     cards: String,
 }
 
 impl Hand {
-    fn score(&self) -> u32 {
-        // 7 score types
-        // 13 card types
-
-        let crd = vec![
-            '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
-        ];
-
-        let mut map = HashMap::new();
-        // println!("{}", self.cards);
-        for kind in crd {
-            for c in self.cards.chars() {
-                if c == kind {
-                    *map.entry(kind).or_insert(0u32) += 1;
-                }
-            }
+    fn tally(&self) -> [u8; 13] {
+        let mut tally = [0u8; 13];
+        for c in self.cards.chars() {
+            let idx = match c {
+                '2' => 0,
+                '3' => 1,
+                '4' => 2,
+                '5' => 3,
+                '6' => 4,
+                '7' => 5,
+                '8' => 6,
+                '9' => 7,
+                'T' => 8,
+                'J' => 9,
+                'Q' => 10,
+                'K' => 11,
+                'A' => 12,
+                _ => unreachable!(),
+            };
+            tally[idx] += 1;
         }
 
-        let mut values = map.iter().map(|(k, v)| *v).collect_vec();
-        values.sort();
+        tally
+    }
+    fn score_part1(&self) -> u32 {
+        let values = self.tally();
 
-        // dbg!(&values);
-
+        // Figure out the ordering of the hand
         if values.contains(&5) {
             return 7;
         }
@@ -56,31 +61,72 @@ impl Hand {
 
         1
     }
-}
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+
+    fn score_part2(&self) -> u32 {
+        let mut values = self.tally();
+        let joker_count = values[9];
+
+        // set jokers to 0
+        values[9] = 0;
+
+        // sort to find the max value at index 0
+        values.sort();
+
+        // Add the joker to the best score now at the bottom
+        values[12] += joker_count;
+
+        // Figure out the ordering of the hand
+        if values.contains(&5) {
+            return 7;
+        }
+
+        if values.contains(&4) {
+            return 6;
+        }
+
+        if values.contains(&3) {
+            return if values.contains(&2) { 5 } else { 4 };
+        }
+        if values.iter().filter(|v| **v == 2).count() == 2 {
+            return 3;
+        }
+
+        if values.contains(&2) {
+            return 2;
+        }
+
+        1
     }
-}
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let crd = vec![
+    fn cmp_part_1(&self, other: &Self) -> Ordering {
+        let card_value_order = [
             '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
         ];
-        if self.score().cmp(&other.score()) != Ordering::Equal {
-            return self.score().cmp(&other.score());
-        }
 
+        self.score_part1()
+            .cmp(&other.score_part1())
+            .then(self.cmp_by_card_value(other, &card_value_order))
+    }
+
+    fn cmp_part_2(&self, other: &Self) -> Ordering {
+        let card_value_order = [
+            'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
+        ];
+
+        self.score_part2()
+            .cmp(&other.score_part2())
+            .then(self.cmp_by_card_value(other, &card_value_order))
+    }
+
+    fn cmp_by_card_value(&self, other: &Self, card_value_order: &[char]) -> Ordering {
+        let mut ordering = Ordering::Equal;
         for (a, b) in self.cards.chars().zip(other.cards.chars()) {
-            let a = crd.iter().position(|p| *p == a).unwrap();
-            let b = crd.iter().position(|p| *p == b).unwrap();
+            let a = card_value_order.iter().position(|p| *p == a).unwrap();
+            let b = card_value_order.iter().position(|p| *p == b).unwrap();
 
-            if a.cmp(&b) != Ordering::Equal {
-                return a.cmp(&b);
-            }
+            ordering = ordering.then(a.cmp(&b));
         }
 
-        Ordering::Equal
+        ordering
     }
 }
 
@@ -95,11 +141,6 @@ impl FromStr for Hand {
 }
 impl Solution for Day7 {
     fn handle_input(&mut self, input: &str) -> anyhow::Result<()> {
-        //         let input = "32T3K 765
-        // T55J5 684
-        // KK677 28
-        // KTJJT 220
-        // QQQJA 483";
         let mut hands = input
             .lines()
             .map(|line| {
@@ -108,19 +149,29 @@ impl Solution for Day7 {
             })
             .collect_vec();
 
-        hands.sort_by(|a, b| a.0.cmp(&b.0));
-        // hands.reverse();
+        hands.sort_by(|a, b| a.0.cmp_part_1(&b.0));
 
-        let mut ans = 0;
-        for (pos, (hand, bid)) in hands.iter().enumerate() {
-            ans += (pos as u64 + 1) * (*bid as u64);
-        }
+        let part_1_ans: u64 = hands
+            .iter()
+            .enumerate()
+            .map(|(pos, (_hand, bid))| (pos as u64 + 1) * (*bid as u64))
+            .sum();
 
-        // dbg!(hands);
+        self.submit_part1(part_1_ans);
 
-        self.submit_part1(ans);
+        hands.sort_by(|a, b| a.0.cmp_part_2(&b.0));
 
-        // dbg!(&hands);
+        let part_2_ans: u64 = hands
+            .iter()
+            .enumerate()
+            .map(|(pos, (_hand, bid))| (pos as u64 + 1) * (*bid as u64))
+            .sum();
+
+        self.submit_part2(part_2_ans);
+
+        assert_eq!(part_1_ans, 250946742);
+        assert_eq!(part_2_ans, 251824095);
+
         Ok(())
     }
 }
