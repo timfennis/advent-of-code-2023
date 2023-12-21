8218f376bb7ce76f1bfd1b2f5c812a77b78dd925
Tim Fennis <fennis.tim@gmail.com>
2023-12-21 12:53:59 +0100

    Schitzoposting

--- src/year2023/day21.rs
+++ src/year2023/day21.rs
@@ -4,6 +4,7 @@ use crate::puzzle::{Answerable, Solution};
 use ahash::{HashMap, HashSet};
 use itertools::Itertools;
 use std::collections::{BTreeSet, VecDeque};
+use std::ops::Add;
 create_solution!(Day21, 2023, 21);
 
 impl Solution for Day21 {
@@ -11,15 +12,28 @@ impl Solution for Day21 {
         println!("{input}");
 
         self.submit_part1(solve(input, 64));
-        self.submit_part2(solve_part2(input, 458));
+
+        let c_327 = solve(input, 327);
+        let p2_327 = solve_part2(input, 327, 327);
+        assert_eq!(p2_327, c_327);
+
+        let c_458 = solve(input, (327 + 131 + 131));
+        let p2_458 = solve_part2(input, 327, (327 + 131 + 131));
+        assert_eq!(p2_458, c_458);
+
+        let part_2_ans = solve_part2(input, 131 * 2 + 65, 26501365);
+        assert!(part_2_ans < 631366825986889);
+        assert!(part_2_ans < 631366825940621);
+        self.submit_part2(part_2_ans);
         // self.submit_part1(solve_part2(input, 26501365));
         Ok(())
     }
 }
 
-fn solve_part2(input: &str, steps: usize) -> usize {
+fn solve_part2(input: &str, probe_len: usize, steps: usize) -> usize {
     let grid = Grid::from_string(input, |ch| ch != '.');
 
+    // let walls: HashSet<Vec2> = Default::default();
     let walls = grid
         .iter_objects()
         .filter_map(|(pos, ch)| (*ch == '#').then_some(pos))
@@ -35,35 +49,17 @@ fn solve_part2(input: &str, steps: usize) -> usize {
 
     let mut queue: HashSet<Vec2> = Default::default();
 
-    let start = grid.iter_objects().find(|(_, ch)| *ch == 'S').unwrap().0;
+    assert_eq!(steps % grid.width(), start.x as usize);
+    assert_eq!(steps % grid.height(), start.y as usize);
     queue.insert(start);
 
     let mut grid_of_grids: HashMap<Vec2, BTreeSet<Vec2>> = Default::default();
 
-    for steps_taken in 0..720 {
+    for steps_taken in 0..327 {
         println!("{steps_taken}");
         // 26 501 365
         let mut next_queue: HashSet<Vec2> = Default::default();
         for pos in queue.iter() {
-            // TEMP: lots of duplication with the code below
-            {
-                let grid_position = Vec2 {
-                    x: pos.x.div_euclid(grid.width() as i64),
-                    y: pos.y.div_euclid(grid.height() as i64),
-                };
-                let gg = grid_of_grids
-                    .entry(grid_position)
-                    .or_insert(Default::default());
-
-                let adjusted = Vec2 {
-                    x: pos.x.rem_euclid(grid.width() as i64),
-                    y: pos.y.rem_euclid(grid.height() as i64),
-                };
-
-                if !walls.contains(&adjusted) {
-                    gg.insert(adjusted);
-                }
-            }
 
             // Original code
             for nd in [
@@ -84,37 +80,135 @@ fn solve_part2(input: &str, steps: usize) -> usize {
             }
         }
 
+
         queue = next_queue;
     }
+
+    // TEMP: lots of duplication with the code below
+    for pos in queue {
+        let grid_position = Vec2 {
+            x: pos.x.div_euclid(grid.width() as i64),
+            y: pos.y.div_euclid(grid.height() as i64),
+        };
+        let gg = grid_of_grids
+            .entry(grid_position)
+            .or_insert(Default::default());
+
+        let adjusted = Vec2 {
+            x: pos.x.rem_euclid(grid.width() as i64),
+            y: pos.y.rem_euclid(grid.height() as i64),
+        };
+
+        if !walls.contains(&adjusted) {
+            gg.insert(adjusted);
+        }
+    }
+
 
     let possible_configurations = grid_of_grids.values().collect::<HashSet<_>>();
     dbg!(grid_of_grids.values().len());
     dbg!(possible_configurations.len());
 
-    for (a, b) in &grid_of_grids {
-        println!("{} {}", a, b.len());
-    }
+    let get = |x: i64, y: i64| grid_of_grids.get(&(x, y).into()).unwrap_or_else(|| panic!("{} {} has to be filled", x, y)).len();
+
+    // for (a, b) in &grid_of_grids {
+    //     println!("{} {}", a, b.len());
+    // }
 
     // assert_eq!(grid_of_grids.values().len(), 21);
-    assert_eq!(possible_configurations.len(), 13);
+    assert_eq!(possible_configurations.len(), 14);
 
+    //       X
+    //      XXX
+    //     XXXXX
+    //    XXXXXXX
+    //     XXXXX
+    //      XXX
+    //       X
+
+    //       *T*
+    //      *%X%*
+    //     *%XXX%*
+    //     LXXXXXR
+    //     *%XXX%*
+    //      *%X%*
+    //       *B*
     // If i'm moving n steps to the right
     let n = steps;
-    let n_blocks = steps / grid.width();
-    let full_blocks = n_blocks - 1;
-    let full_block_count = full_blocks.pow(2) + (full_blocks - 1).pow(2);
+    let total_blocks_per_direction = steps / grid.width();
+    let full_blocks_per_direction = total_blocks_per_direction - 1;
 
-    0
+    println!("dimension {}x{}", grid.width(), grid.height());
+    println!("steps {steps}");
+    println!("total_blocks_per_direction {total_blocks_per_direction}");
+    println!("full_blocks_per_direction {full_blocks_per_direction}");
+
+    let center_block_count = if total_blocks_per_direction % 2 == 0 {
+        (total_blocks_per_direction - 1).pow(2)
+    } else {
+        (total_blocks_per_direction).pow(2)
+    };
+
+    let odd_block_count = if total_blocks_per_direction % 2 == 0 {
+        (total_blocks_per_direction).pow(2)
+    } else {
+        (total_blocks_per_direction - 1).pow(2)
+    };
+
+
+    println!("center_block_count {center_block_count}");
+    println!("odd_block_count {odd_block_count}");
+    dbg!(
+        // Middle part
+        (center_block_count * get(0, 0)),
+        (odd_block_count * get(0, 1)),
+        // Top and stuff
+        get(-2, 0),
+        get(2, 0),
+        get(0, 2),
+        get(0, -2),
+        // Small parts
+        total_blocks_per_direction * get(-2, -1),
+        total_blocks_per_direction * get(2, -1),
+        total_blocks_per_direction * get(-2, 1),
+        total_blocks_per_direction * get(2, 1),
+        // Big parts
+        full_blocks_per_direction * get(-1, -1),
+        full_blocks_per_direction * get(-1, 1),
+        full_blocks_per_direction * get(1, -1),
+        full_blocks_per_direction * get(1, 1),
+    );
+    let parts = [
+        // Middle part
+        (center_block_count * get(0, 0)),
+        (odd_block_count * get(0, 1)),
+        // Top and stuff
+        get(-2, 0),
+        get(2, 0),
+        get(0, 2),
+        get(0, -2),
+        // Small parts
+        total_blocks_per_direction * get(-2, -1),
+        total_blocks_per_direction * get(2, -1),
+        total_blocks_per_direction * get(-2, 1),
+        total_blocks_per_direction * get(2, 1),
+        // Big parts
+        full_blocks_per_direction * get(-1, -1),
+        full_blocks_per_direction * get(-1, 1),
+        full_blocks_per_direction * get(1, -1),
+        full_blocks_per_direction * get(1, 1),
+    ].iter().sum();
+    parts
 }
 
 fn solve(input: &str, steps: usize) -> usize {
     let grid = Grid::from_string(input, |ch| ch != '.');
 
-    let walls: HashSet<Vec2> = Default::default();
-    // let walls = grid
-    //     .iter_objects()
-    //     .filter_map(|(pos, ch)| (*ch == '#').then_some(pos))
-    //     .collect::<HashSet<_>>();
+    let walls = grid
+        .iter_objects()
+        .filter_map(|(pos, ch)| (*ch == '#').then_some(pos))
+        .collect::<HashSet<_>>();
+    // let walls: HashSet<Vec2> = Default::default();
 
     let mut queue: HashSet<Vec2> = Default::default();
 
@@ -124,9 +218,6 @@ fn solve(input: &str, steps: usize) -> usize {
     let mut lens = Vec::new();
 
     for steps_taken in 0..steps {
-        if steps_taken % 100_000 == 0 {
-            // println!("{steps_taken}");
-        }
 
         // 26 501 365
         let mut next_queue: HashSet<Vec2> = Default::default();
@@ -150,19 +241,15 @@ fn solve(input: &str, steps: usize) -> usize {
 
         queue = next_queue;
         lens.push(queue.len());
-        let diff = lens
-            .iter()
-            .tuple_windows()
-            .last()
-            .map(|(a, b)| b - a)
-            .unwrap_or(0);
-
-        let temp_grid = Grid {
-            width: 200,
-            height: 200,
-            objects: queue.iter().map(|p| (*p, 'O')).collect_vec(),
-        };
     }
+
+    // let t = Grid {
+    //     width: grid.width(),
+    //     height: grid.height(),
+    //     objects: queue.iter().map(|p| (*p, 'O')).collect_vec(),
+    // };
+    //
+    // println!("{t}");
 
     queue.len()
 }
@@ -186,7 +273,8 @@ fn calculate_occupied_square_count(f: usize) -> usize {
 
 #[cfg(test)]
 mod test {
-    use crate::year2023::day21::{calculate_occupied_square_count, solve};
+    use crate::year2023::day21::{calculate_occupied_square_count, solve, solve_part2};
+
 
     #[test]
     fn year_2023_day_21_square_count() {
@@ -198,6 +286,7 @@ mod test {
             702_322_399_865_956
         );
     }
+
     #[test]
     fn year_2023_day_21_example_brute_force() {
         let input = "...........
@@ -214,6 +303,7 @@ mod test {
 
         assert_eq!(solve(input, 6), 16);
         assert_eq!(solve(input, 10), 50);
+        assert_eq!(solve_part2(input, 11 * 2 + 5, 5000), 16733044);
         // assert_eq!(solve(input, 50), 1594);
         // assert_eq!(solve(input, 100), 6536);
         // assert_eq!(solve(input, 500), 167004);
